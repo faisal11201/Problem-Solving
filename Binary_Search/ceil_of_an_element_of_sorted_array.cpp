4ca13351b286c59d3ca7c02c62117d96480a56d7
Faisal Khan <59735707+faisal11201@users.noreply.github.com>
2022-02-18 12:04:50 +0530

    Add files via upload

---
+++ Binary_Search/ceil_of_an_element_of_sorted_array.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int ceil_of_array(int arr[],int n,int target){
+    int si = 0;
+    int ei = n-1;
+    int ans = -1;
+    while(si<=ei){
+        int mid = (si+ei)/2;
+        if (arr[mid]==target){
+            return arr[mid];
+        }
+        else if(arr[mid]<target){
+            si = mid+1;
+        }
+        else{
+            ans = arr[mid];
+            ei=mid-1;
+        }
+    }
+    return ans;
+}
+int main(){
+    int arr[7] = {1,2,3,4,8,10,12};
+    int target = 5;
+    cout<<ceil_of_array(arr,7,target);
+
+}
+
+

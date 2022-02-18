179f0b413177955da702ec492269a46a60946ff3
Faisal Khan <59735707+faisal11201@users.noreply.github.com>
2022-02-18 11:53:38 +0530

    Add files via upload

---
+++ Binary_Search/floor_of_an_element.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int floor_of_array(int arr[],int n,int target){
+    int si = 0;
+    int ei = n-1;
+    int ans = -1;
+    while(si<=ei){
+        int mid = (si+ei)/2;
+        if (arr[mid]==target){
+            return arr[mid];
+        }
+        else if(arr[mid]<target){
+            ans = mid;
+            si = mid+1;
+        }
+        else{
+            ei=mid-1;
+        }
+    }
+}
+int main(){
+    int arr[7] = {1,2,3,4,8,10,12};
+    int target = 7;
+    cout<<floor_of_array(arr,7,target);
+
+}
+
+

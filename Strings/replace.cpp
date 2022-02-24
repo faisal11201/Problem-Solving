8a5ca8ae8a8f534bb8b65e97dab4b1785d3301b1
Faisal Khan <59735707+faisal11201@users.noreply.github.com>
2022-02-24 20:10:36 +0530

    Update replace.cpp

--- Strings/replace.cpp
+++ Strings/replace.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+// TC->O(n), SC->O(n)
 string replace_white_space(string s){
     int n = s.length();
     string temp="";
@@ -14,6 +15,14 @@ string replace_white_space(string s){
     }
     return temp;
 }
+// Inplace solution
+replace_white_space(string s){
+    for(int i=0;i<s.length();i++){
+        if(s[i]==' '){
+            s= s.substr(0,i)+"@40"+s.substr(i+1,s.length());
+        }
+    }
+}// no need of return here 
 int main()
 {
     string s;
